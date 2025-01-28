62224fe0c18fb3ec88fdfb0780acc22954253474
roman <romanhordiienko@proton.me>
2025-01-28 16:13:40 +0200

    Add header, pages and routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import CatalogPage from "../pages/CatalogPage/CatalogPage";
+import Header from "./Header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
