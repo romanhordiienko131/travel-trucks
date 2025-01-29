befc696676de314738bf2457b9f3cfe7bac17c98
roman <romanhordiienko@proton.me>
2025-01-29 17:16:38 +0200

    Add container and update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import s from "./Header.module.css";
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
+import Container from "../Container/Container";
 
 const Header = () => {
   return (
     <header className={s.header}>
-      <Logo />
-      <div className={s.navigationContainer}>
-        <Navigation />
-      </div>
+      <Container>
+        <div className={s.headerContainer}>
+          <Logo />
+          <div className={s.navigationContainer}>
+            <Navigation />
+          </div>
+        </div>
+      </Container>
     </header>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
 import s from "./HomePage.module.css";
+import Container from "../../components/Container/Container";
 
 const HomePage = () => {
   return (
     <div className={s.background}>
-      <div>
-        <div className={s.titleWrapper}>
-          <h1 className={s.title}>Campers of your dreams</h1>
-          <p className={s.subTitle}>
-            You can find everything you want in our catalog
-          </p>
+      <Container>
+        <div>
+          <div className={s.titleWrapper}>
+            <h1 className={s.title}>Campers of your dreams</h1>
+            <p className={s.subTitle}>
+              You can find everything you want in our catalog
+            </p>
+          </div>
+          <Link to="/catalog" className={s.link}>
+            View Now
+          </Link>
         </div>
-        <Link to="/catalog" className={s.link}>
-          View Now
-        </Link>
-      </div>
+      </Container>
     </div>
   );
 };
