728288f0d1bb0035e700e3d53aee1adbe3a3588a
roman <romanhordiienko@proton.me>
2025-01-29 15:42:27 +0200

    Update home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import s from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={s.background}>
+      <div>
+        <div className={s.titleWrapper}>
+          <h1 className={s.title}>Campers of your dreams</h1>
+          <p className={s.subTitle}>
+            You can find everything you want in our catalog
+          </p>
+        </div>
+        <Link to="/catalog" className={s.link}>
+          View Now
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
